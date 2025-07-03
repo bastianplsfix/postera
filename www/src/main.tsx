b4bc0a7b604a0d36eba58beb6141510ba66f52d8
bastianplsfix <sebastian.nomme@icloud.com>
2025-07-03 16:01:03 +0200

    added mocking

--- www/src/main.tsx
+++ www/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
 
 // Import the generated route tree
-import { routeTree } from "./routeTree.gen";
+import { routeTree } from "./routeTree.gen.ts";
 
 // Create a new router instance
 const router = createRouter({ routeTree });
@@ -15,13 +15,28 @@ declare module "@tanstack/react-router" {
   }
 }
 
+async function enableMocking() {
+  if (import.meta.env.MODE !== "development") {
+    return;
+  }
+
+  const { worker } = await import("./tests/mocks/browser.ts");
+
+  // `worker.start()` returns a Promise that resolves
+  // once the Service Worker is up and ready to intercept requests.
+  return worker.start();
+}
+
 // Render the app
 const rootElement = document.getElementById("root")!;
 if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
-  root.render(
-    <StrictMode>
-      <RouterProvider router={router} />
-    </StrictMode>,
-  );
+
+  enableMocking().then(() => {
+    root.render(
+      <StrictMode>
+        <RouterProvider router={router} />
+      </StrictMode>,
+    );
+  });
 }
