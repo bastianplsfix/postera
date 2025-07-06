58b48e6f3ee3843fd5a74c889f4712a70bb6d6f6
bastianplsfix <sebastian.nomme@icloud.com>
2025-07-06 20:54:41 +0200

    Update playground.tsx

--- www/src/routes/playground.tsx
+++ www/src/routes/playground.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router"
 import React from "react"
-import { Dialog as BaseDialog } from "@base-ui-components/react/dialog"
+import { Dialog } from "@base-ui-components/react/dialog"
 
 export const Route = createFileRoute("/playground")({
 	component: RouteComponent,
@@ -8,33 +8,35 @@ export const Route = createFileRoute("/playground")({
 
 function RouteComponent() {
 	const [open, setOpen] = React.useState(false)
+
 	return (
 		<div>
-			<div onClick={() => setOpen(true)}>
-				Hello world
-				<button type="button">
-					Open
-				</button>
-			</div>
-			<dialog
-				open={open}
-			>
-				<div className="fixed inset-0 bg-blue-100/50 overflow-y-auto">
-					<div className="relative top-4/5 w-full max-h-[90%] w-full h-full p-2">
-						<div className="bg-blue-400 w-full h-full rounded-lg p-6 shadow-top">
-							The gentle hum of the city faded as dusk settled over the skyline,
-							casting long shadows across the quiet streets. In the soft glow of
-							streetlights, people hurried home, their footsteps echoing on the
-							pavement. Above, the first stars began to appear, promising a calm
-							night ahead. The air was filled with the scent of blooming flowers
-							from nearby gardens, mingling with the distant aroma of fresh
-							bread from a local bakery. It was a moment of peaceful transition,
-							where the bustle of the day gave way to the tranquility of
-							evening.
+			Hello World
+
+			<Dialog.Root open={open} onOpenChange={setOpen}>
+				<Dialog.Trigger>Open</Dialog.Trigger>
+				<Dialog.Portal>
+					<Dialog.Popup>
+						<div className="fixed inset-0 bg-blue-100/50 overflow-y-auto">
+							<div className="relative top-4/5 w-full max-h-[90%] w-full h-full p-2">
+								<div className="bg-blue-400 w-full h-full rounded-lg p-6 shadow-top">
+									<Dialog.Title>Example dialog</Dialog.Title>
+									<Dialog.Close>Close</Dialog.Close>
+									The gentle hum of the city faded as dusk settled over the
+									skyline, casting long shadows across the quiet streets. In the
+									soft glow of streetlights, people hurried home, their
+									footsteps echoing on the pavement. Above, the first stars
+									began to appear, promising a calm night ahead. The air was
+									filled with the scent of blooming flowers from nearby gardens,
+									mingling with the distant aroma of fresh bread from a local
+									bakery. It was a moment of peaceful transition, where the
+									bustle of the day gave way to the tranquility of evening.
+								</div>
+							</div>
 						</div>
-					</div>
-				</div>
-			</dialog>
+					</Dialog.Popup>
+				</Dialog.Portal>
+			</Dialog.Root>
 		</div>
 	)
 }
