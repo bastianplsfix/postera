946a12a0bf5ea17805a8a70633e7f2cca66979f4
bastianplsfix <sebastian.nomme@icloud.com>
2025-07-06 20:59:10 +0200

    Update playground.tsx

--- www/src/routes/playground.tsx
+++ www/src/routes/playground.tsx
@@ -1,6 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router"
 import React from "react"
 import { Dialog } from "@base-ui-components/react/dialog"
+import { Input } from "@base-ui-components/react/input"
 
 export const Route = createFileRoute("/playground")({
 	component: RouteComponent,
@@ -17,9 +18,15 @@ function RouteComponent() {
 				<Dialog.Trigger>Open</Dialog.Trigger>
 				<Dialog.Portal>
 					<Dialog.Popup>
-						<div className="fixed inset-0 bg-blue-100/50 overflow-y-auto">
+						<div
+							className="fixed inset-0 bg-blue-100/50 overflow-y-auto"
+							onClick={() => setOpen(false)}
+						>
 							<div className="relative top-4/5 w-full max-h-[90%] w-full h-full p-2">
-								<div className="bg-blue-400 w-full h-full rounded-lg p-6 shadow-top">
+								<div
+									className="bg-blue-400 w-full h-full rounded-lg p-6 shadow-top"
+									onClick={(e) => e.stopPropagation()}
+								>¯
 									<Dialog.Title>Example dialog</Dialog.Title>
 									<Dialog.Close>Close</Dialog.Close>
 									The gentle hum of the city faded as dusk settled over the
@@ -31,6 +38,7 @@ function RouteComponent() {
 									mingling with the distant aroma of fresh bread from a local
 									bakery. It was a moment of peaceful transition, where the
 									bustle of the day gave way to the tranquility of evening.
+									<Input />
 								</div>
 							</div>
 						</div>
